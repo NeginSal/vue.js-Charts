1ce1dd2e40a030cdde28c5da73933e50ad0a47b5
NeginSal <neginsaljooghi@gmail.com>
2021-07-12 16:41:45 +0430

    change component chart1

--- src/views/Chart1.vue
+++ src/views/Chart1.vue
@@ -1,14 +1,51 @@
 <template>
   <div>
-      
+       <div id="chart">
+        <apexchart type="radialBar" height="350" :options="chartOptions" :series="series"></apexchart>
+      </div>
   </div>
 </template>
 
-<script>
-export default {
 
-}
+<script>
+
+export default {
+  data: function() {
+      return {
+
+          series: [33],
+          chartOptions: {
+            chart: {
+              height: 350,
+              type: 'radialBar',
+            },
+            plotOptions: {
+              radialBar: {
+                hollow: {
+                  size: '33%',
+                }
+              },
+            },
+            labels: ['Cricket'],
+          },
+
+
+
+
+
+
+
+
+
+      }
+    },
+};
+
+
+
 </script>
+
+
 
 <style>
 
